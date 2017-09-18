c8c7b51331db14e778b644c10d92c69dfe57c37a
sarthakmeh <sarthakmeh03@gmail.com>
2017-09-18 23:10:53 +0530

    Hashtags added in response

--- serializers.py
+++ serializers.py
@@ -4,7 +4,7 @@ from models import Image,User
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ('image_id', 'url', 'user', 'created_at', 'description', 'orientation')
+        fields = ('image_id', 'url', 'user', 'created_at', 'description', 'hashtags', 'orientation')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
